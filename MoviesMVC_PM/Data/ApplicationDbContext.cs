3752cafbbd42c31c502b5b327840b2a4c38d8d0e
Derek Peacock <derek_peacock@yahoo.com>
2022-10-20 14:52:15 +0100

    Add project files.

---
+++ MoviesMVC_PM/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoviesMVC_PM.Models;
+
+namespace MoviesMVC_PM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MoviesMVC_PM.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MoviesMVC_PM/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesMVC_PM.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        [Range(0, 30)]
+        public decimal Price { get; set; }
+    }
+}
